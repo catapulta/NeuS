4930ed5d4bc3c6ee0275d9261ba66c76ed150566
Aldo Marini <aldo.marini@noodle.ai>
2022-07-23 10:05:39 -0700

    set colmap num_threads to -1

--- preprocess_custom_data/colmap_preprocess/colmap_wrapper.py
+++ preprocess_custom_data/colmap_preprocess/colmap_wrapper.py
@@ -62,7 +62,7 @@ def run_colmap(basedir, match_type):
             '--database_path', os.path.join(basedir, 'database.db'),
             '--image_path', os.path.join(basedir, 'images'),
             '--output_path', os.path.join(basedir, 'sparse'), # --export_path changed to --output_path in colmap 3.6
-            '--Mapper.num_threads', '16',
+            '--Mapper.num_threads', '-1',
             '--Mapper.init_min_tri_angle', '4',
             '--Mapper.multiple_models', '0',
             '--Mapper.extract_colors', '0',
